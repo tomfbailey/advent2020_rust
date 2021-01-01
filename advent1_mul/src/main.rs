02bfb88b4911085d8ccbb125b8e579bcb755601d
tfb19 <tfb19@ic.ac.uk>
2021-01-01 20:53:17 +0000

    Wrote code for part 2 using HashMap for O(n^2) performance rather than O(n^3)

--- advent1_mul/src/main.rs
+++ advent1_mul/src/main.rs
@@ -1,10 +1,52 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+use std::collections::HashMap;
 
 const SUM_TO_CHECK: i32 = 2020;
 
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
+}
+
+fn part1(v: &Vec<i32>) -> i32 {
+    let mut exit = false;
+    let mut mul = 0;
+    for i in 0..(v.len()-1) {
+        for j in (i+1)..(v.len()) {
+            let sum = v[i] + v[j];
+            if sum == SUM_TO_CHECK {
+                exit = true;
+                println!("1: {}, 2: {}", v[i], v[j]);
+                mul = v[i] * v[j];
+                break;
+            }
+        }
+        if exit {
+            break;
+        }
+    }
+    mul
+}
+
+fn part2(v: &Vec<i32>) -> usize {
+    let mut sums = HashMap::new();
+    for i in 0..(v.len()-1) {
+        for j in (i+1)..(v.len()) {
+            let sum = v[i] + v[j];
+            sums.insert(sum, (v[i], v[j]));
+        }
+    }
+    let mut ret: usize = 0;
+    for i in 0..(v.len()) {
+        let sum = SUM_TO_CHECK - v[i];
+        if sums.contains_key(&sum) {
+            let (a, b) = sums.get(&sum).unwrap();
+            println!("a: {}, b: {}, c: {}", a, b, v[i]);
+            ret = (*a as usize) * (*b as usize) * (v[i] as usize);
+            break;
+        }
+    }
+    ret
 }
 
 fn main() -> io::Result<()>{
@@ -17,54 +59,9 @@ fn main() -> io::Result<()>{
         let line: i32 = line?.parse().expect("Could not parse line");
         v.push(line);
     }
-
-    let mut exit = false;
-    let mut mul = 0;
-    for i in 0..(v.len()-1) {
-        for j in (i+1)..(v.len()) {
-            let sum = v[i] + v[j];
-            if sum == SUM_TO_CHECK {
-                exit = true;
-                println!("1: {}, 2: {}", v[i], v[j]);
-                mul = v[i] * v[j];
-                break;
-            }
-        }
-        if exit {
-            break;
-        }
-    }
-    println!("Answer is {}", mul);
+    println!("-----------PART 1-----------");
+    println!("Product: {}", part1(&v));
+    println!("-----------PART 2-----------");
+    println!("Product: {}", part2(&v));
     Ok(())
-    /*
-    let lines : &str = read_to_string("input.txt")
-                .expect("File not found!")
-                .lines()
-                .collect();
-    print_type_of(&lines);
-    for line in lines {
-        print_type_of(&line);
-        //let line = line.parse::<u32>().expect("Not a number!");
-        v.push(line);
-    }*/
-
-    /*
-    let mut exit = false;
-    let mut mul = 0;
-    for i in 0..(v.len()-1) {
-        for j in (i+1)..(v.len()) {
-            let sum = v[i] + v[j];
-            if sum == SUM_TO_CHECK {
-                exit = true;
-                println!("1: {}, 2: {}", v[i], v[j]);
-                mul = v[i] * v[j];
-                break;
-            }
-        }
-        if exit {
-            break;
-        }
-    }
-    println!("Answer is {}", mul);
-    */
 }
